735c9f9cf4076980d390bac34ae28e1991a585b4
Lakshithatest <dinushanlakshi123@gmail.com>
2025-01-11 11:30:53 +0530

    Update 2022Q1.php

--- 2022Q1.php
+++ 2022Q1.php
@@ -1,6 +1,6 @@
 <?php
-    $_Sql = mysqli_conncet('Localhost', 'userName', 'password', 'emp_db');
+    $_Sql = mysqli_connect('Localhost', 'userName', 'password', 'emp_db');
 
-    $_input = 'INSERT INTO emp_db (Employee_ID, NIC, Title, First_Name, Last_Name, Gender, Date_of_Birth, Age, Salary) 
-    VALUES ( 'empId', 'nic', 'title', 'firstName', 'lastName', 'gender', 'DateOfBirth', 'age', 'salary')';
+    $_input = "INSERT INTO emp_db (Employee_ID, NIC, Title, First_Name, Last_Name, Gender, Date_of_Birth, Age, Salary) 
+    VALUES ('$empId', '$nic', '$title', '$firstName', '$lastName', '$gender', '$DateOfBirth', '$age', '$salary')";
 ?>
