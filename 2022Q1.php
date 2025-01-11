f1059f5a30d3de44af533bd73936124fc341c2bb
Lakshithatest <dinushanlakshi123@gmail.com>
2025-01-11 11:34:05 +0530

    Update 2022Q1.php

--- 2022Q1.php
+++ 2022Q1.php
@@ -3,4 +3,5 @@
 
     $_input = "INSERT INTO emp_db (Employee_ID, NIC, Title, First_Name, Last_Name, Gender, Date_of_Birth, Age, Salary) 
     VALUES ('$empId', '$nic', '$title', '$firstName', '$lastName', '$gender', '$DateOfBirth', '$age', '$salary')";
+    //පුක දෙන්න එ​පා
 ?>
